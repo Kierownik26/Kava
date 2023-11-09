9a0ae339b252ba4be44922b9ec7db9c594a71ed8
Cezary C <cezary@random.grade>
2023-11-09 07:38:30 +0100

    v(randomGrade=true)

---
+++ src/main/java/oop/Collections.java
@@ -0,0 +1,34 @@
+package oop;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+    static String[] tablica;
+    static List<String> lista = new ArrayList<>();
+    static List<String> lista2 = Arrays.asList("aa","bb");
+
+    public static void main(String[] args) {
+        tablica = new String[3];
+        String str1 = "Ala ";
+        String str2 = "ma ";
+        String str3 = "kota";
+        tablica[0] = str1;
+        tablica[1] = str2;
+        tablica[2] = str3;
+
+        for (String s : tablica) {
+            System.out.println(s);
+        }
+
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.println("wow! " + counter);
+        }
+        int counter = 1;
+        while (counter <= 10) {
+            System.out.println("help! " + counter);
+            counter++;
+        }
+    }
+}
